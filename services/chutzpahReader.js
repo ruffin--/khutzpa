bae389252ce523d4956005c8db54e92ddf0772e2
Ruffin <git@rufwork.com>
2023-05-01 11:17:10 -0400

    Method renames, comment cleanup, add simplest case testing method.

--- services/chutzpahReader.js
+++ services/chutzpahReader.js
@@ -17,7 +17,7 @@ var selectorUtils = {
     findAllIncludes: function (selector, theseFiles) {
         var allMatches = [];
 
-        selectorUtils.cleanIncludes(selector);
+        selectorUtils.normalizeIncludeVsIncludes(selector);
 
         console.log("includes", selector.Includes);
 
@@ -77,7 +77,7 @@ var selectorUtils = {
         return allMatches;
     },
 
-    cleanExcludes: function (selector) {
+    normalizeExcludeVsExcludes: function (selector) {
         if (selector.Exclude && !selector.Excludes) {
             selector.Excludes = selector.Exclude;
         }
@@ -90,11 +90,14 @@ var selectorUtils = {
         }
     },
 
-    cleanIncludes: function (selector) {
+    normalizeIncludeVsIncludes: function (selector) {
         // TODO: The Chutzpah docs say these are both plural,
         // but I'm seeing json files with, eg, Include instead of
         // Includes.
         // https://github.com/mmanela/chutzpah/wiki/references-setting
+        //
+        // Same with excludes, above.
+        //
         // I'm not supporting both; this privileges Include singular over
         // Includes plural, erasing plural if singular exists. Is that smart?
         //
@@ -112,7 +115,7 @@ var selectorUtils = {
     },
 
     removeAllExcludes: function (selector, theseFiles) {
-        selectorUtils.cleanExcludes(selector);
+        selectorUtils.normalizeExcludeVsExcludes(selector);
 
         console.log("Excludes", selector.Excludes);
 
@@ -164,18 +167,16 @@ function handleChutzpahSelector(selector, jsonFileParent, type, nth) {
                     selectorFullPath.length - 1
                 );
             }
-            theseFiles = fileSystemService.getAllFilePaths(selectorFullPath);
 
-            // TODO: Do I need to do this?
-            theseFiles = theseFiles || [];
+            // TODO: This will throw on a bogus path (but we checkd with existsSync)
+            // and return [] on an empty folder.
+            theseFiles = fileSystemService.getAllFilePaths(selectorFullPath);
 
             // When we minimatch, we want to do it relative to the parent
             // path.
             // Otherwise "*.js" will never match anything, since we
             // have full paths in the getAllFilePaths results.
             theseFiles = theseFiles.map((x) => x.replace(selectorFullPath, ""));
-
-            theseFiles = theseFiles || [];
 
             console.log(`all files for ${nth}th ${type} selector before filtering: 
     ${selectorFullPath}
@@ -199,19 +200,7 @@ ${JSON.stringify(theseFiles, null, "  ")}
                         
 `);
         } else {
-            // I'm not sure how to handle single files in the root.
-            // Seems they should start with `/` to match the logic.
-            // But I could see doing it either way.
-            // Problem is minimatch acts differently with each, though
-            // they're functionally equivalent.
-            //
-            // TODO: Windows solution
-            // Let's be offensive for now:
-            if (!selector.Path.startsWith("/")) {
-                throw `Paths must start with / ---- #${selector.Path}#`;
-            }
-
-            theseFiles = [jsonFileParent + selector.Path];
+            theseFiles = [nodePath.join(jsonFileParent, selector.Path)];
         }
 
         return theseFiles;
@@ -221,6 +210,13 @@ ${JSON.stringify(theseFiles, null, "  ")}
     return [];
 }
 
+// AggressiveStar means a value of "*.js" looks for "*.js" in EVERY folder,
+// recurisvely rather than simply for files that match ONLY at the root level folder.
+// Some glob evaluators seem to do this and some don't. (???)
+// See, eg, https://github.com/isaacs/minimatch/issues/172#issuecomment-1359582179
+// minimatch (lib we're using for glob selection) doesn't but Chutzpah's glob lib did,
+// so we need to translate. And by "translate", I mean add "**/[pattern with single star]"
+// to the selectors (Include or Exclude as appropriate) to match Chutzpah's glob expectations.
 function handleAggressiveStar(configInfo) {
     var ocdDryPropNames = ["References", "Tests"];
     var ocdDryArrayNames = ["Includes", "Excludes"];
@@ -230,46 +226,49 @@ function handleAggressiveStar(configInfo) {
             ? configInfo[refsOrTests]
             : [configInfo[refsOrTests]];
 
-        configInfo[refsOrTests].forEach((singleEntry) => {
-            selectorUtils.cleanIncludes(singleEntry);
-            selectorUtils.cleanExcludes(singleEntry);
+        configInfo[refsOrTests].forEach((singleRefOrTestEntry) => {
+            selectorUtils.normalizeIncludeVsIncludes(singleRefOrTestEntry);
+            selectorUtils.normalizeExcludeVsExcludes(singleRefOrTestEntry);
 
-            ocdDryArrayNames.forEach((pathArrayName) => {
-                // We don't require that there's an In/Excludes for a single file.
-                if (singleEntry[pathArrayName]) {
-                    singleEntry[pathArrayName] = Array.isArray(singleEntry[pathArrayName])
-                        ? singleEntry[pathArrayName]
-                        : [singleEntry[pathArrayName]];
+            ocdDryArrayNames.forEach((includesOrExcludes) => {
+                // There's no requirement that single files have In- and Ex-cludes so truthycheck 'em.
+                if (singleRefOrTestEntry[includesOrExcludes]) {
+                    singleRefOrTestEntry[includesOrExcludes] = Array.isArray(
+                        singleRefOrTestEntry[includesOrExcludes]
+                    )
+                        ? singleRefOrTestEntry[includesOrExcludes]
+                        : [singleRefOrTestEntry[includesOrExcludes]];
 
                     var toAdd = [];
-                    singleEntry[pathArrayName].forEach((singlePath) => {
+                    singleRefOrTestEntry[includesOrExcludes].forEach((singlePath) => {
+                        // Here's where we translate "*.js" to "**/*.js" to approximate
+                        // Chutzpah's glob selection logic.
                         if (singlePath.startsWith("*") && !singlePath.startsWith("**")) {
-                            // AggressiveStar means a value of "*.js" looks for "*.js" in EVERY folder,
-                            // recurisvely rather than simply at the root level folder.
-                            // Some glob evaluators seem to do this and some don't. (???)
-                            // TODO: Add github issue link.
                             var allFoldersSelector = "**/" + singlePath;
                             if (
-                                !singleEntry[pathArrayName].find(
+                                !singleRefOrTestEntry[includesOrExcludes].find(
                                     (x) => x === allFoldersSelector
                                 )
                             ) {
                                 console.log(
                                     `GOING AGGRESSIVE!!! ${refsOrTests} - ${singlePath}`
                                 );
+
+                                // Note that we're not deleting the old entry but adding the
+                                // superset. Cute, I guess.
                                 toAdd.push(allFoldersSelector);
                             }
                         }
                     });
 
-                    singleEntry[pathArrayName] = singleEntry[pathArrayName].concat(toAdd);
+                    singleRefOrTestEntry[includesOrExcludes] =
+                        singleRefOrTestEntry[includesOrExcludes].concat(toAdd);
                 }
             });
         });
     });
 }
 
-var re = /^\*\/|\/\*\/|\/\*$/g;
 function starReplacer(match /*, p1, offset, original, group*/) {
     // I know, I could've done this less bullheadedly. This works.
     switch (match) {
@@ -282,17 +281,18 @@ function starReplacer(match /*, p1, offset, original, group*/) {
     }
 }
 
+var re = /^\*\/|\/\*\/|\/\*$/g;
 function coverageAggressiveStar(s) {
     return s.replace(re, starReplacer);
 }
 
 function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
-    if (chutzpahConfigObj.AggressiveStar) {
+    if (!chutzpahConfigObj.NoAggressiveStar) {
         handleAggressiveStar(chutzpahConfigObj);
     }
 
+    // GET ALL REFERENCE FILES (files needed to run stuff)
     var allRefFilePaths = [];
-
     chutzpahConfigObj.References.forEach(function (singleReferenceEntry, i) {
         var filesForSelector = handleChutzpahSelector(
             singleReferenceEntry,
@@ -314,6 +314,7 @@ function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
         x.replace(jsonFileParent.replace(/\\/g, "/"), "")
     );
 
+    // GET ALL FILES THAT HAVE TESTS TO RUN
     var specFiles = [];
     if (!singleTestFile) {
         chutzpahConfigObj.Tests.forEach(function (ref, i) {
@@ -322,6 +323,7 @@ function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
             specFiles = mergeAndDedupe(specFiles, filesForSelector);
         });
     } else {
+        // TODO: not sure what the commented out stuff was doing.
         // var parentAtStart = `^${jsonFileParent}`;
         // var reParentAtStart = new RegExp(parentAtStart, "i");
         // var singleFileWithParentRemoved = singleTestFile.replace(reParentAtStart, "");
@@ -336,11 +338,13 @@ function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
         throw "No files to test! " + JSON.stringify(chutzpahConfigObj.Tests);
     }
 
+    // REMOVE ANY SPEC FILES FOUND IN REFERENCES
     // Chutzpah doesn't seem to make a distinction between what should be referenced
     // and what should be tested -- or, more specifically, you can ref tests without
     // causing an issue (I think?).
     // Let's remove any test files from our ref files so they're not duplicated.
-    // (I mean, I guess it'd work with them, but you get the point.)
+    // (I mean, I guess it'd work with them, but you get the point that tests shouldn't
+    //  be coverage tested.)
     allRefFilePaths = allRefFilePaths.filter((path) => specFiles.indexOf(path) === -1);
 
     var coverageFiles = [];
@@ -351,7 +355,9 @@ function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
             coverageIncludePattern = coverageIncludePattern.replace(/\\/g, "/");
             // }
 
-            if (chutzpahConfigObj.AggressiveStar) {
+            if (!chutzpahConfigObj.NoAggressiveStar) {
+                // TODO: This doesn't seem to be written for single file entries.
+                // ????: Why wouldn't logic for Ref/Spec In/Excludes work?
                 coverageIncludePattern = coverageAggressiveStar(coverageIncludePattern);
             }
 
@@ -362,6 +368,8 @@ function parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile) {
             console.log(coverageIncludePattern, coverageFiles);
         });
     }
+
+    // TODO: CodeCoverageExcludes
 
     return {
         allRefFilePaths,
@@ -375,11 +383,9 @@ function findChutzpahJson(startPath) {
         throw `Invalid start path: ${startPath}`;
     }
 
-    // ????: Should we care about other types? Should we
-    // be ready for any extension? This is kinda offensively programmed.
-    var possibleDir = startPath.toLowerCase().endsWith(".js")
-        ? nodePath.dirname(startPath)
-        : startPath;
+    var statsObj = fs.statSync(startPath);
+    // If it's a file, grab the parent dir.
+    var possibleDir = statsObj.isDirectory() ? startPath : nodePath.dirname(startPath);
 
     var foundChutzpahJson = undefined;
     while (!foundChutzpahJson) {
@@ -418,38 +424,49 @@ function getConfigInfo(originalTestPath) {
     var jsonFilePath = nodePath.normalize(configFilePath);
     var jsonFileParent = nodePath.dirname(jsonFilePath);
 
-    // TODO: Or just check if it ends with `.js` or `.ts`?
     var singleTestFile =
-        fs.statSync(originalTestPath).isDirectory() &&
-        originalTestPath.lastIndexOf(".") > originalTestPath.indexOf(slash) &&
-        !originalTestPath.toLowerCase().endsWith("chutzpah.json")
+        originalTestPath.toLowerCase().endsWith(".js") ||
+        originalTestPath.toLowerCase().endsWith(".ts")
             ? originalTestPath
             : false;
 
-    // If you execute from an "inner" directory, don't include test files
-    // outside of that path. (*DO* include all ref files, though.)
-    var limitingDir = singleTestFile
-        ? nodePath.dirname(originalTestPath)
-        : originalTestPath;
-
-    return fileSystemService.getFileContents(jsonFilePath).then(function (chutzpahJso) {
-        var chutzpahConfigObj = JSON.parse(chutzpahJso);
+    return fileSystemService.getFileContents(jsonFilePath).then(function (chutzpahJson) {
+        var chutzpahConfigObj = JSON.parse(chutzpahJson);
         console.log("read chutzpah json", chutzpahConfigObj);
 
-        var ret = parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile);
+        var info = parseChutzpahInfo(chutzpahConfigObj, jsonFileParent, singleTestFile);
 
         return {
             originalTestPath,
             configFilePath,
             jsonFileParent,
-            allRefFilePaths: ret.allRefFilePaths,
-            specFiles: ret.specFiles,
-            coverageFiles: ret.coverageFiles,
+            allRefFilePaths: info.allRefFilePaths,
+            specFiles: info.specFiles,
+            coverageFiles: info.coverageFiles,
         };
     });
 }
 
 module.exports = {
     getConfigInfo,
-    findChutzpahJson, // okay, this is just here for testing. That's bad.
+    findChutzpahJson, // okay, this was made public only for testing. That's bad.
 };
+
+if (require.main === module) {
+    // First two are always "Node" and the path to what was called.
+    // Trash those.
+    const myArgs = process.argv.slice(2);
+    console.log("myArgs: ", myArgs);
+
+    fileSystemService
+        .getFileContents("C:\\temp\\chutzpahTestValues.json")
+        .then(function (jsonContents) {
+            var chutzpahTestValues = JSON.parse(jsonContents);
+
+            getConfigInfo(chutzpahTestValues.configPath).then((values) => {
+                var valuesAsString = JSON.stringify(values, null, "  ");
+                console.log(valuesAsString);
+                fs.writeFileSync("C:\\temp\\values.json", valuesAsString);
+            });
+        });
+}
