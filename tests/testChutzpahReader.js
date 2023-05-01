bc772a5c1a92fde2600a550b956c16bd0df8b28a
Ruffin <git@rufwork.com>
2023-05-01 11:41:34 -0400

    This test file has become a mess. Ignore it for now.

--- tests/testChutzpahReader.js
+++
@@ -1,8 +0,0 @@
-const configReader = require("../services/chutzpahReader");
-
-var configPath = process.argv[2];
-
-configReader.getConfigInfo(configPath).then((values) => {
-    console.log(JSON.stringify(values, null, "  "));
-    debugger;
-});
