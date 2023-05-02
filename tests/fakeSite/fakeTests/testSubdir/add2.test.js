4df4a78cef50d5badf85c5c73ad8bae1c82d714b
Ruffin <git@rufwork.com>
2023-05-01 23:48:05 -0400

    Add back [purposefully] broken test.

--- tests/fakeSite/fakeTests/testSubdir/add2.test.js
+++ tests/fakeSite/fakeTests/testSubdir/add2.test.js
@@ -13,4 +13,17 @@ describe("these are not real tests", function () {
             expect(result).toBe(7);
         });
     });
+
+    describe("add2broken function in global scope", function () {
+        it("should return a value that is 2 greater than that which was pushed in", function () {
+            // arrange
+            var x = 5;
+
+            // act
+            var result = window.add2broken(x);
+
+            // assert
+            expect(result).toBe(7);
+        });
+    });
 });
