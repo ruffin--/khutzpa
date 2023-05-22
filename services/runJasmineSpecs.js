85d312ae5a384c7e0be49b56c3a81b6c9c3757e8
OpenHawkes <104998366+openHawkes@users.noreply.github.com>
2023-05-22 15:13:53 -0400

    Add concept of root dir for serving files to createSpecHtml.

--- services/runJasmineSpecs.js
+++ services/runJasmineSpecs.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const fsPromises = fs.promises;
 const nodePath = require("node:path");
+
 const utils = require("../helpers/utils");
 
 // https://jasmine.github.io/pages/getting_started.html
-var specRunnerTemplate = `<!DOCTYPE html>
+const scriptTemplate = `<script src="1"></script>\n`;
+const specRunnerTemplate = `<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -26,19 +28,16 @@ var specRunnerTemplate = `<!DOCTYPE html>
 </html>
 `;
 
-function createSpecHtml(configInfo, writeToFile) {
+function createSpecHtml(configInfo, writeToFile, root) {
     // Note that all paths from configInfo will be full paths.
-    var scriptTagsToInsert = configInfoToTestRunnerScript(configInfo);
+    var scriptTagsToInsert = configInfoToTestRunnerScript(configInfo, root);
 
     // So I won't be using tildes in my test scripts.
     // https://github.com/nodejs/node/issues/684
     var htmlContents = specRunnerTemplate.replace("{0}", scriptTagsToInsert);
 
     if (writeToFile) {
-        var runnerPath = nodePath.join(
-            nodePath.dirname(configInfo.configFilePath),
-            "runner.html"
-        );
+        var runnerPath = nodePath.join(root, "runner.html");
         utils.debugLog(`${htmlContents}
 
 #############
@@ -63,18 +62,22 @@ writing to
     });
 }
 
-var scriptTemplate = `<script src="1"></script>\n`;
-function configInfoToTestRunnerScript(configInfo) {
+function configInfoToTestRunnerScript(configInfo, root) {
     // Create script include section for head of output page.
     var forRunner = "<!-- include source files here -->\n";
 
     function insertToTemplate(filePath) {
-        forRunner += scriptTemplate
-            .replace("1", filePath)
-            // TODO: Need more than just this. Relative paths should be checked.
-            // So if we're running two up and then back down, we want
-            // `../../whatever/file.js`
-            .replace(`src="${configInfo.jsonFileParent}`, `src="`);
+        if (filePath.indexOf("jasmine") > -1 && filePath.endsWith(".js")) {
+            console.warn(`
+!!!!!! ${filePath}
+contains the characters "jasmine". khutzpa provides its own version of jasmine.
+Referencing another version of jasmine can break tests. Currently skipping this file.
+Note: There is currently no way to override this check.
+
+TODO: Allow overriding this check.`);
+        } else {
+            forRunner += scriptTemplate.replace("1", nodePath.relative(root, filePath));
+        }
     }
 
     configInfo.allRefFilePaths.forEach(insertToTemplate);
@@ -83,6 +86,36 @@ function configInfoToTestRunnerScript(configInfo) {
     configInfo.specFiles.forEach(insertToTemplate);
 
     return forRunner;
+}
+
+// TODO: Remove integration testing so we can remove these two requires.
+const fileSystemService = require("./fileSystemService");
+const chutzpahReader = require("./chutzpahReader");
+
+if (require.main === module) {
+    // First two are always "Node" and the path to what was called.
+    // Trash those.
+    const myArgs = process.argv.slice(2);
+    utils.debugLog("myArgs: ", myArgs);
+
+    fileSystemService.getFileContents("C:\\temp\\chutzpahTestValues.json").then(
+        (testValueFileContents) => {
+            var testConfigPath = JSON.parse(testValueFileContents).singleChutzpah;
+
+            fileSystemService.getFileContents(testConfigPath).then(
+                chutzpahReader.getConfigInfo(testConfigPath).then(
+                    (values) => {
+                        createSpecHtml(values, !"don't write to a file").then((html) => {
+                            utils.debugLog(html.runnerHtml);
+                        });
+                    },
+                    (err) => console.error("2", err)
+                ),
+                (err) => console.error("1", err)
+            );
+        },
+        (err) => console.error("0", err)
+    );
 }
 
 module.exports = {
