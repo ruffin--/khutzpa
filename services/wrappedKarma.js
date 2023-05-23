4b8228bc9caeee9e7afc6160ea1bd32d9e931662
Ruffin <git@rufwork.com>
2023-05-23 16:39:43 -0400

    Working Promise management for runWrappedKarma.

--- services/wrappedKarma.js
+++ services/wrappedKarma.js
@@ -12,75 +12,74 @@ let karmaRunIds = [];
 let karmaRunResults = {};
 
 function startKarma(karmaRunId, overrides) {
-    overrides = Object.assign(
-        {},
-        karmaConfigTools.overridesForMochaTestingRun,
-        overrides
-    );
-    var karmaConfig = karmaConfigTools.createKarmaConfig(overrides);
-
-    karmaConfig.files.forEach((x, i) => {
-        if (stringManipulation.isString(x) && stringManipulation.startsWithSlash(x)) {
-            karmaConfig.files[i] = x.substring(1);
-        } else if (
-            x.pattern &&
-            stringManipulation.isString(x.pattern) &&
-            stringManipulation.startsWithSlash(x.pattern)
-        ) {
-            karmaConfig.files[i].pattern = x.pattern.substring(1);
-        }
-    });
-
-    var processorKeys = Object.keys(karmaConfig.preprocessors);
-    processorKeys.forEach((key) => {
-        if (stringManipulation.startsWithSlash(key)) {
-            karmaConfig.preprocessors[key.substring(1)] = ["coverage"];
-            delete karmaConfig.preprocessors[key];
-        }
-    });
-    // logit(karmaConfig);
-
-    var karmaPromise = karma.config
-        .parseConfig(
-            null,
-            karmaConfig,
-
-            // In most cases, parseOptions.throwErrors = true should also be set.
-            // This disables process exiting and allows errors to result in rejected promises.
-            // http://karma-runner.github.io/6.3/dev/public-api.html
-            { promiseConfig: true, throwErrors: true }
-        )
-        .then(
-            (parsedKarmaConfig) => {
-                // fwiw
-                // http://karma-runner.github.io/6.4/dev/public-api.html
-                // I'm not sure why it names the callback function doneCallback.
-                const server = new karma.Server(parsedKarmaConfig, function doneCallback(
-                    exitCode
-                ) {
-                    // 0 is success/no test failure.
-                    // Anything else is bad. Usually 1 afaict.
-                    utils.debugLog("Wrapped karma has exited with " + exitCode);
-                    karmaRunResults[karmaRunId] = exitCode;
-
-                    return exitCode;
-                }).on("progress", function (data) {
-                    process.stdout.write(data);
-                });
-
-                var serverPromise = server.start();
-                return serverPromise.then(function (x) {
-                    utils.debugLog("server promise", x);
-                    return x;
-                });
-            },
-            (rejectReason) => {
-                utils.debugLog("Error", rejectReason);
-                throw rejectReason;
-            }
+    return new Promise(function (resolve, reject) {
+        overrides = Object.assign(
+            {},
+            karmaConfigTools.overridesForMochaTestingRun,
+            overrides
         );
+        var karmaConfig = karmaConfigTools.createKarmaConfig(overrides);
 
-    return karmaPromise;
+        karmaConfig.files.forEach((x, i) => {
+            if (stringManipulation.isString(x) && stringManipulation.startsWithSlash(x)) {
+                karmaConfig.files[i] = x.substring(1);
+            } else if (
+                x.pattern &&
+                stringManipulation.isString(x.pattern) &&
+                stringManipulation.startsWithSlash(x.pattern)
+            ) {
+                karmaConfig.files[i].pattern = x.pattern.substring(1);
+            }
+        });
+
+        var processorKeys = Object.keys(karmaConfig.preprocessors);
+        processorKeys.forEach((key) => {
+            if (stringManipulation.startsWithSlash(key)) {
+                karmaConfig.preprocessors[key.substring(1)] = ["coverage"];
+                delete karmaConfig.preprocessors[key];
+            }
+        });
+        // logit(karmaConfig);
+
+        karma.config
+            .parseConfig(
+                null,
+                karmaConfig,
+
+                // In most cases, parseOptions.throwErrors = true should also be set.
+                // This disables process exiting and allows errors to result in rejected promises.
+                // http://karma-runner.github.io/6.3/dev/public-api.html
+                { promiseConfig: true, throwErrors: true }
+            )
+            .then(
+                (parsedKarmaConfig) => {
+                    // fwiw
+                    // http://karma-runner.github.io/6.4/dev/public-api.html
+                    // I'm not sure why it names the callback function doneCallback.
+                    const server = new karma.Server(
+                        parsedKarmaConfig,
+                        function doneCallback(exitCode) {
+                            // 0 is success/no test failure.
+                            // Anything else is bad. Usually 1 afaict.
+                            utils.debugLog("Wrapped karma has exited with " + exitCode);
+                            karmaRunResults[karmaRunId] = exitCode;
+
+                            resolve(exitCode);
+                        }
+                    ).on("progress", function (data) {
+                        process.stdout.write(data);
+                    });
+
+                    server.start().then(function (x) {
+                        utils.debugLog("server started", x);
+                    });
+                },
+                (rejectReason) => {
+                    utils.debugLog("Error", rejectReason);
+                    throw rejectReason;
+                }
+            );
+    });
 }
 
 function runWrappedKarma(configInfo, karmaRunId) {
@@ -128,65 +127,29 @@ if (require.main === module) {
     karmaRunIds.push(karmaRunId);
     karmaRunResults[karmaRunId] = undefined;
 
-    if (myArgs[0]) {
-        var configResult = {
-            jsonFileParent: myArgs[1] || "C:\\projects\\khutzpa\\tests\\fakeSite",
-            allRefFilePaths: [
-                "\\fakeCode\\add2.js",
-                "\\fakeCode\\double.js",
-                "\\fakeCode\\square.js",
-            ],
-            specFiles: [
-                "\\fakeCode\\double.test.js",
-                "\\fakeTests\\code.test.js",
-                "\\fakeTests\\testSubdir\\add2.test.js",
-            ],
-            coverageFiles: [
-                "\\fakeCode\\add2.js",
-                "\\fakeCode\\angular",
-                "\\fakeCode\\double.js",
-                "\\fakeCode\\square.js",
-            ],
-        };
+    var configResult = {
+        jsonFileParent: myArgs[0] || "./tests/fakeSite",
+        allRefFilePaths: [
+            "/fakeCode/add2.js",
+            "/fakeCode/double.js",
+            "/fakeCode/square.js",
+        ],
+        specFiles: [
+            "/fakeCode/double.test.js",
+            "/fakeTests/code.test.js",
+            "/fakeTests/testSubdir/add2.test.js",
+        ],
+        coverageFiles: [
+            "/fakeCode/add2.js",
+            "/fakeCode/angular",
+            "/fakeCode/double.js",
+            "/fakeCode/square.js",
+        ],
+    };
 
-        runWrappedKarma(configResult, karmaRunId).then(function (exitCode) {
-            utils.debugLog("done (Promises probably outstanding)", exitCode);
-        });
-    } else {
-        var config = {
-            karmaRunId: "probably a chutzpah.json path",
-            port: 9876,
-            basePath: "C:\\projects\\khutzpa\\tests\\fakeSite",
-            files: [
-                "\\fakeCode\\add2.js",
-                "\\fakeCode\\double.js",
-                "\\fakeCode\\square.js",
-                "\\fakeCode\\double.test.js",
-                "\\fakeTests\\code.test.js",
-                "\\fakeTests\\testSubdir\\add2.test.js",
-            ],
-            preprocessors: {
-                "\\fakeCode\\add2.js": ["coverage"],
-                "\\fakeCode\\angular": ["coverage"],
-                "\\fakeCode\\double.js": ["coverage"],
-                "\\fakeCode\\square.js": ["coverage"],
-            },
-        };
-
-        // startKarma(config).then(function (after) {
-        //     utils.debugLog("after", after);
-        // });
-
-        startKarma(karmaRunId, config);
-
-        // TODO: Turn into a Promise.
-        var intervalId = setInterval(function () {
-            if (karmaRunIds.every((x) => karmaRunResults[x] !== undefined)) {
-                utils.debugLog(karmaRunResults, "global exit codes");
-                clearInterval(intervalId);
-            }
-        }, 2000);
-    }
+    runWrappedKarma(configResult, karmaRunId).then(function (exitCode) {
+        utils.debugLog("done (Promises probably outstanding)", exitCode);
+    });
 }
 
 module.exports = {
