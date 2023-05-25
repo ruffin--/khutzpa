3b75df42efc8c57fbcccc82c93a78af7fa0e158b
Ruffin <git@rufwork.com>
2023-05-25 18:00:58 -0400

    Remove express for coverage page. Properly Promise-ify runKarmaCoverage.
    maybe.

--- services/coverage.js
+++ services/coverage.js
@@ -5,7 +5,6 @@ const nodePath = require("node:path");
 const opener = require("opener");
 
 const karmaConfigTools = require("./karmaConfigTools");
-const expressServer = require("./expressServer");
 const utils = require("../helpers/utils");
 
 function startKarma(overrides) {
@@ -13,98 +12,79 @@ function startKarma(overrides) {
     var karmaConfig = karmaConfigTools.createKarmaConfig(overrides);
     utils.debugLog(karmaConfig);
 
-    var serverHasStarted = false;
+    return new Promise(function (resolve, reject) {
+        karma.config
+            .parseConfig(
+                null,
+                karmaConfig,
 
-    return karma.config
-        .parseConfig(
-            null,
-            karmaConfig,
+                // In most cases, parseOptions.throwErrors = true should also be set.
+                // This disables process exiting and allows errors to result in rejected promises.
+                // http://karma-runner.github.io/6.3/dev/public-api.html
+                { promiseConfig: true, throwErrors: true }
+            )
+            .then(
+                (parsedKarmaConfig) => {
+                    // fwiw
+                    // http://karma-runner.github.io/6.4/dev/public-api.html
+                    // I'm not sure why it names the callback function.
+                    const server = new Server(parsedKarmaConfig, function doneCallback(
+                        exitCode
+                    ) {
+                        utils.debugLog("Karma has exited with " + exitCode);
+                        utils.debugLog(arguments);
 
-            // In most cases, parseOptions.throwErrors = true should also be set.
-            // This disables process exiting and allows errors to result in rejected promises.
-            // http://karma-runner.github.io/6.3/dev/public-api.html
-            { promiseConfig: true, throwErrors: true }
-        )
-        .then(
-            (parsedKarmaConfig) => {
-                // fwiw
-                // http://karma-runner.github.io/6.4/dev/public-api.html
-                // I'm not sure why it names the callback function.
-                const server = new Server(parsedKarmaConfig, function doneCallback(
-                    exitCode
-                ) {
-                    utils.debugLog("Karma has exited with " + exitCode);
-                    utils.debugLog(arguments);
+                        var coverageDir = nodePath.join(karmaConfig.basePath, "coverage");
 
-                    var coverageDir = nodePath.join(karmaConfig.basePath, "coverage");
+                        fs.readdir(coverageDir, function (err, list) {
+                            var latestCoverageDir = "";
+                            var latestTime = 0;
+                            utils.debugLog(list, err);
 
-                    fs.readdir(coverageDir, function (err, list) {
-                        var latestCoverageDir = "";
-                        var latestTime = 0;
-                        utils.debugLog(list, err);
-
-                        list.forEach((file) => {
-                            // TODO: Change when we have other browsers, natch.
-                            if (file.indexOf("Chrome") > -1) {
-                                var fullPath = nodePath.join(coverageDir, file);
-                                var statsObj = fs.statSync(fullPath);
-                                if (statsObj.isDirectory()) {
-                                    utils.debugLog(`
+                            list.forEach((file) => {
+                                // TODO: Change when we have other browsers, natch.
+                                if (file.indexOf("Chrome") > -1) {
+                                    var fullPath = nodePath.join(coverageDir, file);
+                                    var statsObj = fs.statSync(fullPath);
+                                    if (statsObj.isDirectory()) {
+                                        utils.debugLog(`
 path: ${fullPath}
 last accessed: ${statsObj.atimeMs},
 last changed:  ${statsObj.ctimeMs},
 last modified: ${statsObj.mtimeMs},
                                 `);
-                                }
+                                    }
 
-                                if (statsObj.ctimeMs > latestTime) {
-                                    latestTime = statsObj.ctimeMs;
-                                    latestCoverageDir = fullPath;
+                                    if (statsObj.ctimeMs > latestTime) {
+                                        latestTime = statsObj.ctimeMs;
+                                        latestCoverageDir = fullPath;
+                                    }
                                 }
+                            });
+
+                            if (latestCoverageDir) {
+                                utils.debugLog(latestCoverageDir);
+                                var coverageFilePath = nodePath.join(
+                                    latestCoverageDir,
+                                    "index.html"
+                                );
+                                opener(coverageFilePath);
+
+                                resolve(0);
+                            } else {
+                                reject(new Error("No coverage directory found!"));
                             }
                         });
-
-                        if (latestCoverageDir) {
-                            utils.debugLog(latestCoverageDir);
-
-                            // TODO: Figure out why doneCallback is getting called twice.
-                            if (!serverHasStarted) {
-                                serverHasStarted = true;
-                                var serverApp =
-                                    expressServer.startPassthrough(latestCoverageDir);
-
-                                var expressPort = 3000;
-                                serverApp.listen(expressPort, function () {
-                                    utils.debugLog(
-                                        `Example app listening on port ${expressPort}!`
-                                    );
-                                });
-
-                                var handle = opener(
-                                    `http://localhost:${expressPort}/index.html`
-                                );
-                                utils.debugLog("handle pid: " + handle.pid);
-
-                                console.warn(
-                                    "Please note that stopping this process can replace " +
-                                        "your coverage report with a 404 error. Please stop on when you're ready."
-                                );
-                            }
-                        } else {
-                            console.warn("No coverage directory found!");
-                        }
                     });
 
-                    // process.exit(exitCode);
-                });
-
-                server.start();
-            },
-            (rejectReason) => {
-                /* respond to the rejection reason error */
-                console.error("Error", rejectReason);
-            }
-        );
+                    server.start();
+                },
+                (rejectReason) => {
+                    /* respond to the rejection reason error */
+                    console.error("Error", rejectReason);
+                }
+            );
+    });
 }
 
 function runKarmaCoverage(configInfo) {
